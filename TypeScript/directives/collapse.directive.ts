bd8b6e32638f66b631923c63255c1d6583a6d6e0
sv0505 <sv0505@gmail.com>
2017-02-16 16:33:41 -0500

    Update collapse.directive.ts

--- TypeScript/directives/collapse.directive.ts
+++ TypeScript/directives/collapse.directive.ts
@@ -3,68 +3,38 @@ import {Directive, Input, HostBinding} from '@angular/core';
 
 @Directive({selector: '[collapse]'})
 export class Collapse {
-  // style
-  @HostBinding('style.height')
-  private height:string;
-  // shown
-  @HostBinding('class.in')
-  @HostBinding('attr.aria-expanded')
-  private isExpanded:boolean = true;
-  // hidden
-  @HostBinding('attr.aria-hidden')
-  private isCollapsed:boolean = false;
-  // stale state
-  @HostBinding('class.collapse')
-  private isCollapse:boolean = true;
-  // animation state
-  @HostBinding('class.collapsing')
-  private isCollapsing:boolean = false;
+
+  @HostBinding("class.collapse.show")
+  private isShow:boolean = true;
+
+  @HostBinding("class.collapse")
+  private isHide:boolean = false;
 
   @Input()
   private set collapse(value:boolean) {
-    this.isExpanded = value;
-    this.toggle();
-  }
-
-  private get collapse():boolean {
-    return this.isExpanded;
+    this.toggle(value);
   }
 
   constructor() {
   }
 
-  toggle() {
-    if (this.isExpanded) {
-      this.hide();
-    } else {
+  toggle(value: boolean) {
+    if (value) {
       this.show();
+    } else {
+      this.hide();
     }
   }
 
   hide() {
-    this.isCollapse = false;
-    this.isCollapsing = true;
-
-    this.isExpanded = false;
-    this.isCollapsed = true;
-    setTimeout(() => {
-      this.height = '0';
-      this.isCollapse = true;
-      this.isCollapsing = false;
-    }, 4);
+    this.isShow = false;
+    this.isHide = true;
+    console.log('hide');
   }
 
   show() {
-    this.isCollapse = false;
-    this.isCollapsing = true;
-
-    this.isExpanded = true;
-    this.isCollapsed = false;
-    setTimeout(() => {
-      this.height = 'auto';
-
-      this.isCollapse = true;
-      this.isCollapsing = false;
-    }, 4);
+    this.isShow = true;
+    this.isHide = false;
+    console.log('show');
   }
 }
