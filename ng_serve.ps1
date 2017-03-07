0e4c8f0b327e2b245284ff7ca71c4e9910f3891f
sv0505 <sv0505@gmail.com>
2017-03-07 13:53:38 -0500

    Delete ng_serve.ps1

--- ng_serve.ps1
+++
@@ -1,6 +0,0 @@
-[string]$newFileName = "c:\\prg\\ng_" + [DateTime]::Now.ToString("yyyyMMdd-HHmmss") + ".log";
-
-cd C:\Dev\Balboa\Main\Code\wwwroot\Corporatek.Web.UI\ui
-
-ng serve | tee $newFileName
-
