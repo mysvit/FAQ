6d76f70d78f5b4c158e716728eee1775cf02283c
sv0505 <sv0505@gmail.com>
2017-03-24 14:32:33 -0400

    Update recursiveAPICall.ts

--- TypeScript/recursiveAPICall.ts
+++ TypeScript/recursiveAPICall.ts
@@ -1,7 +1,7 @@
   getRecursiveCall(valueID: Muid): Observable<any> {
   
     let callAndMap = (id) =>
-      this.xtBroker.locate(IApiActFlowService).GetApiFlowAction(id)
+      this.http.locate(IApiActFlowService).GetApiFlowAction(id)
       .map(res => {
           return {id: res.id, data: res}
       });  // map, and save the page number for recursion later.
