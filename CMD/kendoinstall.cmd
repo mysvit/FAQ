c3504c408849c29da3d69649db0b9b9f72c813fd
sv0505 <sv0505@gmail.com>
2017-05-05 09:38:26 -0400

    Create kendoinstall.cmd

---
+++ CMD/kendoinstall.cmd
@@ -0,0 +1,68 @@
+rem Buttons 1
+npm install --save @progress/kendo-angular-buttons @progress/kendo-angular-l10n
+
+rem Charts 1
+npm install --save @progress/kendo-angular-charts @progress/kendo-angular-intl @progress/kendo-angular-l10n @progress/kendo-drawing hammerjs
+
+rem Data Query 1
+npm install --save @progress/kendo-data-query
+
+rem DateInputs 1
+npm install --save @progress/kendo-angular-dateinputs @progress/kendo-angular-intl @progress/kendo-angular-l10n
+
+rem Dialog 1
+npm install --save @progress/kendo-angular-dialog @progress/kendo-angular-l10n
+
+rem Drawing 1
+npm install --save @progress/kendo-drawing
+
+rem DropDowns 1
+npm install --save @progress/kendo-angular-dropdowns @progress/kendo-angular-l10n
+
+rem Excel Export 1
+npm install --save @progress/kendo-angular-excel-export @progress/kendo-angular-l10n
+
+rem File Saver 1
+npm install --save @progress/kendo-file-saver
+
+rem Grid 1
+npm install --save @progress/kendo-angular-grid @progress/kendo-angular-dropdowns @progress/kendo-angular-inputs @progress/kendo-angular-dateinputs @progress/kendo-data-query @progress/kendo-angular-intl @progress/kendo-angular-l10n @progress/kendo-drawing @progress/kendo-angular-excel-export
+
+rem Inputs 1
+npm install --save @progress/kendo-angular-inputs @progress/kendo-angular-intl @progress/kendo-angular-l10n
+
+rem Internationalization
+rem -----------------------------
+
+rem Label 1
+npm install --save @progress/kendo-angular-label
+
+rem Localization
+rem -----------------------------
+
+rem Layout 1
+npm install --save @progress/kendo-angular-layout @progress/kendo-angular-l10n
+
+rem Popup 1
+npm install --save @progress/kendo-angular-popup
+
+rem ScrollView 1
+npm install --save @progress/kendo-angular-scrollview
+
+rem Sortable 1
+npm install --save @progress/kendo-angular-sortable @progress/kendo-angular-l10n
+
+rem Upload 1
+npm install --save @progress/kendo-angular-upload @progress/kendo-angular-l10n
+
+rem Styling 1
+npm install --save @progress/kendo-theme-default
+
+rem Intl 1
+@progress/kendo-angular-intl
+
+rem l10n 1
+@progress/kendo-angular-l10n
+
+
+rem npm install --save '@telerik/kendo-intl';
