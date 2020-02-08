56c7c5513c621ae1cf8c6bae8fc2500126c18e7e
sv0505 <sv0505@gmail.com>
2020-02-07 21:54:08 -0500

    Update extract-audio.sh

--- linux/ffmpeg/extract-audio.sh
+++ linux/ffmpeg/extract-audio.sh
@@ -1,6 +1,5 @@
-ffmpeg -i input-video.avi -vn -acodec copy output-audio.aac
-
 // extract ADTS audio
 ffmpeg -i input-video.avi -vn -acodec copy output-audio.aac
+
 // transcode stereo
 ffmpeg -i input-video.avi -map 0:1 -c:a aac ukr.m4a
