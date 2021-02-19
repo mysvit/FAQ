9899ac8c329de020528ae5735ffbf4b32938101f
mysvit <sv0505@gmail.com>
2021-02-19 16:56:47 -0500

    Update stat.sh

--- OpenWrt/stat.sh
+++ OpenWrt/stat.sh
@@ -9,7 +9,7 @@ opkg install tcpdump-mini
 cat /proc/net/nf_conntrack | grep tcp
 
 #restart dhcp
-/etc/init.d/adblock restart
+/etc/init.d/dnsmasq restart
 /etc/init.d/odhcpd restart
 #resatr addblock
 /etc/init.d/adblock restart
