a018f5b6ea71c9a43a8907ca13f1f0e371a74ee4
mysvit <sv0505@gmail.com>
2021-08-02 10:46:54 -0400

    Create commands.sh
    Get CPU Temperature

---
+++ linux/commands.sh
@@ -0,0 +1,4 @@
+# Get CPU temperature. x86_pkg_temp XX.X°C
+
+paste <(cat /sys/class/thermal/thermal_zone*/type) <(cat /sys/class/thermal/thermal_zone*/temp) | column -s $'\t' -t | sed 's/\(.\)..$/.\1°C/'
+
