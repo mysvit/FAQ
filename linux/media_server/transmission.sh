40a0c3fb7f115b105386e5cc1a181533677c9f90
mysvit <smix@tuta.io>
2022-02-22 08:26:59 -0500

    Delete transmission.sh

--- linux/media_server/transmission.sh
+++
@@ -1,9 +0,0 @@
-
-
-#https://help.ubuntu.com/community/TransmissionHowTo
-
-sudo add-apt-repository ppa:transmissionbt/ppa
-sudo apt-get update
-sudo apt-get install transmission-cli transmission-common transmission-daemon
-sudo service transmission-daemon stop
-sudo nano /var/lib/transmission-daemon/info/settings.json
