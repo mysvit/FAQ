cd7bd96435809756e211fba0742a57b5e6943a58
mysvit <smix@tuta.io>
2022-02-22 08:52:03 -0500

    Update video-info.sh

--- linux/double-screen/video-info.sh
+++ linux/double-screen/video-info.sh
@@ -1,4 +1,10 @@
+############################### nvidia driver info - glxinfo
+
+#run on user permittion not root
+glxinfo | egrep "OpenGL vendor|OpenGL renderer"
+
 # info opengl
 glxinfo | grep NVIDIA
+
 # list of cards
 lspci | grep VGA
