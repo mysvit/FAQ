d27aad112e241090d6b7767a4c35162f6f04996f
mysvit <smix@tuta.io>
2022-02-22 11:07:01 -0500

    Delete get_temp.sh

--- linux/get_temp.sh
+++
@@ -1,4 +0,0 @@
-# Get CPU temperature. x86_pkg_temp XX.X°C
-
-paste <(cat /sys/class/thermal/thermal_zone*/type) <(cat /sys/class/thermal/thermal_zone*/temp) | column -s $'\t' -t | sed 's/\(.\)..$/.\1°C/'
-
