b1b773ff447e3bdef74ab599c1ac90ba566a3a28
mysvit <smix@tuta.io>
2022-06-17 23:35:59 +0300

    Update my-linux.sh

--- linux/my-linux.sh
+++ linux/my-linux.sh
@@ -11,6 +11,8 @@ wget https://download3.operacdn.com/pub/opera/desktop/88.0.4412.40/linux/opera-s
 sudo dpkg -i /tmp/opera-stable_88.0.4412.40_amd64.deb; \
 sudo rm /tmp/opera-stable_88.0.4412.40_amd64.deb
 
+# Webstorm - download and unpack to ~/soft/webstorm
+# from app luncher add to Favorites and TaskManager
 tee -a /home/sv/.local/share/applications/webstorm.desktop <<EOF
 [Desktop Entry]
 Comment[en_US]=
