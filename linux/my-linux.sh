10d949b40955afbfa76dd98cc194fe6dcd30e3b7
mysvit <smix@tuta.io>
2022-06-17 22:39:14 +0300

    Update my-linux.sh

--- linux/my-linux.sh
+++ linux/my-linux.sh
@@ -1,4 +1,3 @@
-wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add -
-sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
-apt-get update
-apt-get install -yq google-chrome-stable
+# Google Chrome
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb -O /tmp/google-chrome-stable_current_amd64.deb; \
+sudo dpkg -i /tmp/google-chrome-stable_current_amd64.deb
