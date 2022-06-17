9fa7542ffe0dd6db789f4e0d6fbf1d3d919ef9eb
mysvit <smix@tuta.io>
2022-06-17 22:49:52 +0300

    Update my-linux.sh

--- linux/my-linux.sh
+++ linux/my-linux.sh
@@ -1,3 +1,8 @@
 # Google Chrome
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb -O /tmp/google-chrome-stable_current_amd64.deb; \
 sudo dpkg -i /tmp/google-chrome-stable_current_amd64.deb
+sudo rm /tmp/google-chrome-stable_current_amd64.deb
+
+wget https://download3.operacdn.com/pub/opera/desktop/88.0.4412.40/linux/opera-stable_88.0.4412.40_amd64.deb -O /tmp/opera-stable_88.0.4412.40_amd64.deb; \
+sudo dpkg -i /tmp/opera-stable_88.0.4412.40_amd64.deb
+sudo rm /tmp/opera-stable_88.0.4412.40_amd64.deb
