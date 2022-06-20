4b99b7f75b0519ef17e41812818d251f035f169a
mysvit <smix@tuta.io>
2022-06-20 11:52:15 +0300

    Update my-linux.sh

--- linux/my-linux.sh
+++ linux/my-linux.sh
@@ -2,16 +2,16 @@ sudo apt-get update;
 sudo apt-get install ca-certificates wget gnupg lsb-release;
 
 # Google Chrome
-wget -qO- https://dl.google.com/linux/linux_signing_key.pub | gpg --dearmor | sudo dd /usr/share/keyrings/google.gpg
+wget -qO- https://dl.google.com/linux/linux_signing_key.pub | gpg --dearmor | sudo dd of=/usr/share/keyrings/google.gpg;
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/google.gpg] http://dl.google.com/linux/chrome/deb/ stable main" \
- | sudo dd /etc/apt/sources.list.d/google-chrome.list;
+ | sudo dd of=/etc/apt/sources.list.d/google-chrome.list;
 sudo apt update;
 sudo apt install -y google-chrome-stable;
 
 # Opera
-wget -qO- https://deb.opera.com/archive.key | gpg --dearmor | sudo dd /usr/share/keyrings/opera-browser.gpg;
+wget -qO- https://deb.opera.com/archive.key | gpg --dearmor | sudo dd of=/usr/share/keyrings/opera-browser.gpg;
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/opera-browser.gpg] https://deb.opera.com/opera-stable/ stable non-free" \
- | sudo dd /etc/apt/sources.list.d/opera-stable.list;
+ | sudo dd of=/etc/apt/sources.list.d/opera-stable.list;
 sudo apt-get update;
 sudo apt-get install opera-stable;
 
@@ -40,9 +40,9 @@ X-KDE-Username=
 EOF
 
 # Docker
-wget -qO- https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor | sudo dd /usr/share/keyrings/docker.gpg;
+wget -qO- https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor | sudo dd of=/usr/share/keyrings/docker.gpg;
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" \
-  | sudo dd /etc/apt/sources.list.d/docker.list;
+  | sudo dd of=/etc/apt/sources.list.d/docker.list;
 sudo apt-get update;
 sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin;
 
