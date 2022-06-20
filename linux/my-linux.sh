de355c6eac6699893212d0c8cfdbcbc12b1c4e83
mysvit <smix@tuta.io>
2022-06-20 20:53:40 +0300

    Update my-linux.sh
    websorm

--- linux/my-linux.sh
+++ linux/my-linux.sh
@@ -1,6 +1,38 @@
 sudo apt-get update;
 sudo apt-get install ca-certificates wget gnupg lsb-release;
 
+
+# Webstorm - download and unpack to ~/soft/webstorm
+mkdir ~/soft;
+wget https://download.jetbrains.com/webstorm/WebStorm-2022.1.2.tar.gz -O ~/soft/WebStorm-2022.1.2.tar.gz;
+mkdir -p ~/soft/webstorm;
+tar -xvf ~/soft/WebStorm-2022.1.2.tar.gz -C ~/soft/webstorm --strip-components=1;
+# from app luncher add to Favorites and TaskManager
+mkdir -P ~/.local/share/applications
+
+tee -a ~/.local/share/applications/webstorm.desktop <<EOF
+[Desktop Entry]
+Comment[en_US]=
+Comment=
+Exec=$HOME/soft/webstorm/bin/webstorm.sh
+GenericName[en_US]=
+GenericName=
+Icon=$HOME/soft/webstorm/bin/webstorm.png
+MimeType=
+Name[en_US]=WebStorm
+Name=WebStorm
+Path=
+StartupNotify=true
+Terminal=false
+TerminalOptions=
+Type=Application
+X-DBUS-ServiceName=
+X-DBUS-StartupType=
+X-KDE-SubstituteUID=false
+X-KDE-Username=
+EOF
+bash ~/soft/webstorm/bin/webstorm.sh
+
 # Google Chrome
 wget -qO- https://dl.google.com/linux/linux_signing_key.pub | gpg --dearmor | sudo dd of=/usr/share/keyrings/google.gpg;
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/google.gpg] http://dl.google.com/linux/chrome/deb/ stable main" \
@@ -34,29 +66,6 @@ sudo apt install firefox
 sudo apt install chromium-browser
 
 
-# Webstorm - download and unpack to ~/soft/webstorm
-# from app luncher add to Favorites and TaskManager
-tee -a /home/sv/.local/share/applications/webstorm.desktop <<EOF
-[Desktop Entry]
-Comment[en_US]=
-Comment=
-Exec=/home/sv/soft/webstorm/bin/webstorm.sh
-GenericName[en_US]=
-GenericName=
-Icon=/home/sv/soft/webstorm/bin/webstorm.png
-MimeType=
-Name[en_US]=WebStorm
-Name=WebStorm
-Path=
-StartupNotify=true
-Terminal=false
-TerminalOptions=
-Type=Application
-X-DBUS-ServiceName=
-X-DBUS-StartupType=
-X-KDE-SubstituteUID=false
-X-KDE-Username=
-EOF
 
 # Docker
 wget -qO- https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor | sudo dd of=/usr/share/keyrings/docker.gpg;
