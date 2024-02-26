e63b0b0dbfd9b9c015581b733a552409a5334aae
Summer Mix <smix@tuta.io>
2024-02-26 08:27:27 -0500

    Delete linux/ffmpeg/S02E08.sh

--- linux/ffmpeg/S02E08.sh
+++
@@ -1,3 +0,0 @@
-ffmpeg -ss 40.3 -i "S02E08 35. Пчелка Майя - Подземная ловушка.mp4" \
- -ss 40.3 -i "S02E08.Sleepless Max.mp4" -map 0:0 -c copy -map 1:1 -c copy -metadata:s:a:0 language=ukr -metadata:s:a:0 title="Ukrainian stereo" \
-"Maya the Bee S02E08 - Sleepless Max.mp4"
